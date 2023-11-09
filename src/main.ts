26d822decdce4e6271216614796fbf269d8fff5e
红光 <2273049646@qq.com>
2023-11-09 23:25:17 +0800

    feat: 初始化项目

---
+++ src/main.ts
@@ -0,0 +1,28 @@
+import { VersioningType, VERSION_NEUTRAL } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { TransformInterceptor } from './common/interceptors/transform.interceptor';
+// 刚换底层框架从 express->fastify
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
+  // 接口版本化管理
+  app.enableVersioning({
+    defaultVersion: [VERSION_NEUTRAL, '1', '2'],
+    type: VersioningType.URI,
+  });
+
+  // 处理通用的返回格式
+  app.useGlobalInterceptors(new TransformInterceptor());
+
+  await app.listen(3000);
+}
+bootstrap();
